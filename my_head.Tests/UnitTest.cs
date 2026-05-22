687ed3fc8b090ea5fd89229751ca3ec7a18b97c3
Arturich666 <karukartur@example.com>
2026-05-22 05:09:45 -1200

    Додано unit-тести

--- my_head.Tests/UnitTest.cs
+++ my_head.Tests/UnitTest.cs
@@ -1,15 +1,84 @@
-﻿namespace my_head.Tests;
+﻿using NUnit.Framework;
+using System;
+using System.IO;
 
-public class Tests
+namespace my_head.Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class AppTests
     {
-    }
+        [Test]
+        public void Run_StdinWithN_OutputsFirstNLines_Returns0()
+        {
+            var input = new StringReader("line1\nline2\nline3\nline4\n");
+            var output = new StringWriter();
+            var error = new StringWriter();
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            int exitCode = App.Run(new[] { "-n", "2" }, input, output, error);
+
+            Assert.That(exitCode, Is.EqualTo(0));
+            Assert.That(output.ToString(), Is.EqualTo($"line1{Environment.NewLine}line2{Environment.NewLine}"));
+            Assert.That(error.ToString(), Is.Empty);
+        }
+
+        [Test]
+        public void Run_InvalidOption_ReturnsCode2()
+        {
+            var input = new StringReader("");
+            var output = new StringWriter();
+            var error = new StringWriter();
+
+            int exitCode = App.Run(new[] { "--unknown-option" }, input, output, error);
+
+            Assert.That(exitCode, Is.EqualTo(2));
+            Assert.That(output.ToString(), Is.Empty);
+            Assert.That(error.ToString(), Does.Contain("unknown").IgnoreCase);
+        }
+
+        [Test]
+        public void Run_MissingNValue_ReturnsCode2()
+        {
+            var input = new StringReader("");
+            var output = new StringWriter();
+            var error = new StringWriter();
+
+            int exitCode = App.Run(new[] { "-n" }, input, output, error);
+
+            Assert.That(exitCode, Is.EqualTo(2));
+            Assert.That(error.ToString(), Does.Contain("invalid number").IgnoreCase);
+        }
+
+        [Test]
+        public void Run_ValidFile_OutputsLines_Returns0()
+        {
+            string tempFile = Path.GetTempFileName();
+            File.WriteAllText(tempFile, "a\nb\nc\n");
+
+            var input = new StringReader("");
+            var output = new StringWriter();
+            var error = new StringWriter();
+
+            int exitCode = App.Run(new[] { "-n", "1", tempFile }, input, output, error);
+
+            Assert.That(exitCode, Is.EqualTo(0));
+            Assert.That(output.ToString(), Is.EqualTo($"a{Environment.NewLine}"));
+            Assert.That(error.ToString(), Is.Empty);
+
+            File.Delete(tempFile);
+        }
+
+        [Test]
+        public void Run_FileNotFound_ReturnsCode1()
+        {
+            var input = new StringReader("");
+            var output = new StringWriter();
+            var error = new StringWriter();
+
+            int exitCode = App.Run(new[] { "nonexistent_file.txt" }, input, output, error);
+
+            Assert.That(exitCode, Is.EqualTo(1));
+            Assert.That(output.ToString(), Is.Empty);
+            Assert.That(error.ToString(), Does.Contain("No such file").IgnoreCase);
+        }
     }
 }
--- my_head/Program.cs
+++ my_head/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace my_head
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return App.Run(args, Console.In, Console.Out, Console.Error);
+        }
+    }
+}
