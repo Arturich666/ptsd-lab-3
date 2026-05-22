68f8da3136cf370fd6f821e085a00ac7b8c961ea
Arturich666 <karukartur@example.com>
2026-05-22 04:33:46 -1200

    Ініціалізація

---
+++ my_head.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace my_head.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
